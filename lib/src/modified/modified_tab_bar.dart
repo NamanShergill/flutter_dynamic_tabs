8869bda17a3f9af93eda6b144d2a655a593232fe
Naman <namanpreet@gmail.com>
2023-10-02 23:44:27 +0530

    moved to new multi info card impl.

--- lib/src/modified/modified_tab_bar.dart
+++ lib/src/modified/modified_tab_bar.dart
@@ -1,7 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, unnecessary_null_comparison
 
 import 'dart:ui';
 
